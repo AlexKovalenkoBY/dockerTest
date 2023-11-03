bc45363594a049b66f2e1059a18bd2b199da4526
AlexKovalenkoBY <kovalenkoam@gmail.com>
2023-11-03 09:05:44 +0300

    add start point to pom

--- Dockerfile
+++ Dockerfile
@@ -2,7 +2,7 @@ FROM eclipse-temurin:17-jdk-alpine as builder
 WORKDIR /opt/app
 COPY ./ ./
 
-# RUN chmod +x ./mvnw
+RUN chmod +x ./mvnw
 RUN ./mvnw dependency:go-offline
 RUN ./mvnw clean install
 
--- src/main/java/com/docker/java/JavaApplication.java
+++ src/main/java/com/docker/java/JavaApplication.java
@@ -4,15 +4,16 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication; 
 import org.springframework.web.bind.annotation.RequestMapping; 
 import org.springframework.web.bind.annotation.RestController; 
-
+import java.util.Date;
 @SpringBootApplication
 @RestController
 public class JavaApplication { 
 
 	@RequestMapping("/") 
-	public String home() { 
-		return "Dockerizing Spring Boot Application"; 
-		// return "/"; 
+	public String home() {
+		String res =  "Dockerizing Spring Boot Application " + new Date().toString();
+		return res;
+	
 	} 
 
 	public static void main(String[] args) { 
